902b0b5f8f05063fbff123ad610c3e856ba7c497
kilianpaquier <107955904+kilianpaquier@users.noreply.github.com>
2025-08-29 16:34:38 +0200

    fix(lint): remove cognit in test files
    Signed-off-by: kilianpaquier <107955904+kilianpaquier@users.noreply.github.com>

--- pkg/generate/parser_glob_test.go
+++ pkg/generate/parser_glob_test.go
@@ -7,10 +7,10 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/kickr-dev/engine/pkg/files"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-	"github.com/kickr-dev/engine/pkg/files"
 	"github.com/kickr-dev/kickr/pkg/generate"
 	"github.com/kickr-dev/kickr/pkg/generate/types"
 )
