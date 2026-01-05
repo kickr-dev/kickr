6573920cca1d236505e6ecd8318fc08078447e3e
kilianpaquier <8428791-u.kilianpaquier@users.noreply.gitlab.com>
2026-01-05 21:37:24 +0100

    fix(kickr): isolate release token from kickr token and add reviewers to layout merge requests on GitLab
    Signed-off-by: kilianpaquier <8428791-u.kilianpaquier@users.noreply.gitlab.com>

--- scripts/sh/update.sh
+++ scripts/sh/update.sh
@@ -22,7 +22,7 @@ for workspace in $workspaces; do
   dirs=$(find "$workspace" -name testdata -prune -o -name .kickr -exec dirname {} +;)
   for dir in $dirs; do
     log_info "Updating layout of $dir"
-    $cmd --dir "$dir" generate --force
+    $cmd --dir "$dir" generate
   done
   unset dirs dir
 done
