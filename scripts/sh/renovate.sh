61d7c632f4c85f2e02c4409061dea1d5574a803b
kilianpaquier <noreply@kilianpaquier.dev>
2026-08-15 19:53:19 +0200

    ci(sh): invalid commands for renovate pull requests testdata updates
    Signed-off-by: kilianpaquier <noreply@kilianpaquier.dev>

--- scripts/sh/renovate.sh
+++ scripts/sh/renovate.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
 install-tool golang "$(grep '^toolchain ' go.mod | awk '{print $2}' | sed 's/^go//')"
-make testdata
-
-make build
-./kickr
+TESTDATA=1 go test ./... -run ^TestGenerate_ -count 1 -timeout=15s
+go run ./cmd/kickr
